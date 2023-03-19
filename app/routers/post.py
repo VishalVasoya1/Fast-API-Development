87d389d6c2eda8afcc4ccf4ff1c7687ec50fb0b8
VishalVasoya1 <vasoyavishal33@gmail.com>
2023-03-19 23:10:44 +0530

    testing for webhook

--- app/routers/post.py
+++ app/routers/post.py
@@ -9,6 +9,7 @@ router = APIRouter(
     tags=['Posts']
 )
 
+# what is the schemas.Post
 # @router.get('/',response_model=List[schemas.Post])
 @router.get('/',response_model=List[schemas.PostOut])
 def get_post(db : Session = Depends(get_db), current_user : int = Depends(oauth2.get_current_user), limit:int=10, skip:int=0, search:Optional[str]=""):
